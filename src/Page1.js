bd5f6ccf4dc0104951458f781230b657df454b39
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2024-10-21 20:21:09 +0800

    implement post list

--- src/Page1.js
+++ src/Page1.js
@@ -1,7 +1,21 @@
+import { useLoaderData, Link } from "react-router-dom";
+
 function Page1() {
+  const loaderData = useLoaderData();
+
   return (
     <div>
-      <h4>Page 1</h4>
+      <h4>List</h4>
+
+      <ul>
+        {loaderData.data.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link to={`/page/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -18,6 +18,20 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Page1 />,
+        loader: async () => {
+          try {
+            const response = await fetch(
+              `https://jsonplaceholder.typicode.com/posts`
+            );
+            const data = await response.json();
+
+            return {
+              data: data,
+            };
+          } catch (err) {
+            console.error(err);
+          }
+        },
       },
       {
         path: "/page/:id",
